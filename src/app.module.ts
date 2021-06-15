0e01c152cde5295a0e6ad15708713aed391ccce5
OxyTom <oxytom8@gmail.com>
2021-06-15 19:28:13 +0200

    bug fixs

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppGateway } from './app.gateway';
 import { AuthModule } from './auth/auth.module';
+import { LogModule } from './log/log.module';
 import { MissionModule } from './mission/mission.module';
 import OrmConfig from './ormconfig';
 import { PlayerModule } from './player/player.module';
 import { RoleModule } from './role/role.module';
 import { TeamModule } from './team/team.module';
-import { LogController } from './log/log.controller';
-import { LogService } from './log/log.service';
-import { LogModule } from './log/log.module';
 
 @Module({
   imports: [
@@ -22,7 +20,7 @@ import { LogModule } from './log/log.module';
     TeamModule,
     LogModule,
   ],
-  controllers: [AppController, LogController],
-  providers: [AppGateway, LogService],
+  controllers: [AppController],
+  providers: [AppGateway],
 })
 export class AppModule {}
--- src/player/player.module.ts
+++ src/player/player.module.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
-import { PlayerService } from './player.service';
 import { PlayerController } from './player.controller';
-import { PlayerListController } from './playerList.controller';
 import { Player } from './player.entity';
+import { PlayerService } from './player.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Player])],
   providers: [PlayerService],
   exports: [PlayerService],
-  controllers: [PlayerController, PlayerListController],
+  controllers: [PlayerController],
 })
 export class PlayerModule {}
