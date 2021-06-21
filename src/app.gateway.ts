5a621c8a2079d8fe5a7cfe87f6a80b280af2728c
OxyTom <oxytom8@gmail.com>
2021-06-21 23:19:24 +0200

    now CRUD with WS

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -1,17 +1,28 @@
+import { InjectRepository } from '@nestjs/typeorm';
 import {
   OnGatewayConnection,
   OnGatewayDisconnect,
   OnGatewayInit,
   SubscribeMessage,
   WebSocketGateway,
-  WebSocketServer,
+  WebSocketServer
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
+import { Repository } from 'typeorm';
+import { Mission } from './mission/mission.entity';
+import { Role } from './role/role.entity';
+import { Team } from './team/team.entity';
 
 @WebSocketGateway({ transport: ['websocket'] })
 export class AppGateway
   implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer() server: Server;
+
+  constructor(
+    @InjectRepository(Mission) private missionRepository: Repository<Mission>,
+    @InjectRepository(Team) private teamRepository: Repository<Team>,
+    @InjectRepository(Role) private roleRepository: Repository<Role>,
+  ) {}
 
   afterInit(server: Server) {
     console.log('WS Init');
@@ -28,57 +39,75 @@ export class AppGateway
   /* MISSION EVENTS */
   @SubscribeMessage('create::mission')
   handleMissionCreate(client: Socket, payload: string): void {
-    //TODO: create mission
+    const { user, mission } = JSON.parse(payload);
+    //TODO: Auth
+    this.missionRepository.create(mission);
     this.server.emit('created::mission', payload);
   }
 
   @SubscribeMessage('edit::mission')
   handleMissionEdit(client: Socket, payload: string): void {
-    //TODO: edit mission
+    const { user, mission } = JSON.parse(payload);
+    //TODO: Auth
+    this.missionRepository.update(mission.id, mission);
     this.server.emit('edited::mission', payload);
   }
 
   @SubscribeMessage('delete::mission')
   handleMissionDelete(client: Socket, payload: string): void {
-    //TODO: delete mission
+    const { user, mission } = JSON.parse(payload);
+    //TODO: Auth
+    this.missionRepository.delete(mission.id);
     this.server.emit('deleted::mission', payload);
   }
 
   /* TEAM EVENTS */
   @SubscribeMessage('create::team')
   handleTeamCreate(client: Socket, payload: string): void {
-    //TODO: add team
+    const { user, team } = JSON.parse(payload);
+    //TODO: Auth
+    this.teamRepository.create(team);
     this.server.emit('created::team', payload);
   }
 
   @SubscribeMessage('edit::team')
   handleTeamEdit(client: Socket, payload: string): void {
-    //TODO: edit team
+    const { user, team } = JSON.parse(payload);
+    //TODO: Auth
+    this.teamRepository.update(team.id, team);
     this.server.emit('edited::team', payload);
   }
 
   @SubscribeMessage('delete::team')
   handleTeamDelete(client: Socket, payload: string): void {
-    //TODO: delete team
+    const { user, team } = JSON.parse(payload);
+    //TODO: Auth
+    this.teamRepository.delete(team.id);
     this.server.emit('deleted::team', payload);
   }
 
   /* ROLE EVENTS */
   @SubscribeMessage('create::role')
   handleRoleCreate(client: Socket, payload: string): void {
-    //TODO: add role
+    const { user, role } = JSON.parse(payload);
+    //TODO: Auth
+    this.roleRepository.create(role);
     this.server.emit('created::role', payload);
   }
 
   @SubscribeMessage('edit::role')
   handleRoleEdit(client: Socket, payload: string): void {
-    //TODO: edit role
+    const { user, role } = JSON.parse(payload);
+    //TODO: Auth
+    this.roleRepository.update(role.id, role);
     this.server.emit('edited::role', payload);
   }
 
   @SubscribeMessage('delete::role')
   handleRoleDelete(client: Socket, payload: string): void {
-    //TODO: delete role
+    const { user, role } = JSON.parse(payload);
+    //TODO: Auth
+    this.roleRepository.delete(role.id);
     this.server.emit('deleted::role', payload);
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,21 @@ import { AppController } from './app.controller';
 import { AppGateway } from './app.gateway';
 import { AuthModule } from './auth/auth.module';
 import { LogModule } from './log/log.module';
+import { Mission } from './mission/mission.entity';
 import { MissionModule } from './mission/mission.module';
 import OrmConfig from './ormconfig';
 import { PlayerModule } from './player/player.module';
+import { Role } from './role/role.entity';
 import { RoleModule } from './role/role.module';
+import { Team } from './team/team.entity';
 import { TeamModule } from './team/team.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(OrmConfig),
+    TypeOrmModule.forFeature([Mission]),
+    TypeOrmModule.forFeature([Team]),
+    TypeOrmModule.forFeature([Role]),
     PlayerModule,
     MissionModule,
     RoleModule,
